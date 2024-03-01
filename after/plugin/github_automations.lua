80363cd369d68c7c90d2373d9e7e41f79005c6cc
Nedzib Sastoque <bsastoque@buk.co>
2024-02-29 22:17:37 -0500

    feat: open last commit of line

---
+++ after/plugin/github_automations.lua
@@ -0,0 +1,33 @@
+function get_git_commit()
+    local filename = vim.fn.expand('%:p')
+    local line_number = vim.fn.line('.')
+
+    local git_blame_output = vim.fn.systemlist("git blame -L " .. line_number .. "," .. line_number .. ' ' .. filename .. ' | grep -Eo "^\\w+" | awk \'{print $1}\'')
+
+    return git_blame_output[1]
+end
+
+function get_remote_url()
+    return vim.fn.systemlist('git config --get remote.origin.url')[1]
+end
+
+function open_github_commit()
+    local github_url = get_remote_url() .. '/commit/' .. get_git_commit()
+    if is_wsl() then
+        vim.fn.jobstart("cmd.exe /c start " .. github_url)
+    else
+        vim.fn.jobstart({'xdg-open', github_url})
+    end
+end
+
+function is_wsl()
+    local file = io.open("/proc/sys/kernel/osrelease", "r")
+    if file then
+        file:close()
+        return true
+    else
+        return false
+    end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>gc', ':lua open_github_commit()<CR>', {noremap = true, silent = true})
